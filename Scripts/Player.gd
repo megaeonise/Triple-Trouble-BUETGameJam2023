c3af2795f9f5d17178b00c100b218d21634db0d6
Rikth Humayun <rikthmeowhumayun@gmail.com>
2023-01-03 13:26:11 +0600

    added default block and player character

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -13,16 +13,40 @@ func get_input(delta):
 	velocity.x = 0
 	if not state in states.slice(2,4):
 		print('it works')
+		#Right Move
 		if Input.is_action_pressed("move_right"):
 			velocity.x += speed
+		#Left Move
 		if Input.is_action_pressed("move_left"):
 			velocity.x -= speed
-		if Input.is_action_just_pressed(("dash")):
-			state = states[2]
-			velocity.x += speed*2.5
+		#Right Dash
+		if Input.is_action_just_pressed("dash")  and Input.is_action_pressed('move_right'):
+			if state == states[0]:
+				print('rdash')
+				state = states[2]
+				velocity.x += speed*2.5
+				state = states[0]
+			else:
+				print('rudash')
+				state = states[2]
+				velocity.x += speed*2.5
+				state = states[1]
+		#Left Dash
+		if Input.is_action_just_pressed("dash") and Input.is_action_pressed('move_left'):
+			if state == states[0]:
+				print('ldash')
+				state = states[2]
+				velocity.x -= speed*2.5
+				state = states[0]
+			else:
+				print('ludash')
+				state = states[2]
+				velocity.x -= speed*2.5
+				state = states[1]
 	#Gravity and Friction
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
 
 func _physics_process(delta):
 	get_input(delta)
+
