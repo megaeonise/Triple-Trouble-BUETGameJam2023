858add62607fb40caeea83e54616289284f8014b
Rikth Humayun <rikthmeowhumayun@gmail.com>
2023-01-05 18:57:41 +0600

    Added diagonal transform

--- Scripts/Grass.gd
+++ Scripts/Grass.gd
@@ -3,6 +3,7 @@ var player_x: int
 var player_y: int
 var floor_block: int
 var wall_block:int
+var diag_block:int
 var direction: bool
 var player: Vector2
 var local: Vector2
@@ -22,6 +23,11 @@ func _process(delta):
 		wall_block = get_cell(local.x-1, local.y)
 	else:
 		wall_block = get_cell(local.x+1, local.y)
+	#Diag Cell
+	if direction:
+		diag_block = get_cell(local.x-1, local.y+1)
+	else:
+		diag_block = get_cell(local.x+1, local.y+1)
 	emit_signal('blocks', floor_block, wall_block)
 
 func _on_Player_Direction(facing):
@@ -57,3 +63,22 @@ func _on_Player_Interact(breaker):
 					set_cell(local.x+1, local.y, 3)
 				elif wall_block==3:
 					set_cell(local.x+1, local.y, 2)
+		elif diag_block!=-1:
+			if direction:
+				if diag_block==0:
+					set_cell(local.x-1, local.y+1, 1)
+				elif diag_block==1:
+					set_cell(local.x-1, local.y+1, 0)
+				elif diag_block==2:
+					set_cell(local.x-1, local.y+1, 3)
+				elif diag_block==3:
+					set_cell(local.x-1, local.y+1, 2)
+			else:
+				if diag_block==0:
+					set_cell(local.x+1, local.y+1, 1)
+				elif diag_block==1:
+					set_cell(local.x+1, local.y+1, 0)
+				elif diag_block==2:
+					set_cell(local.x+1, local.y+1, 3)
+				elif diag_block==3:
+					set_cell(local.x+1, local.y+1, 2)
